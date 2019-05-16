a7e02dccc249c7c0fbdf98368f0171a04bca3dda
Indra <indra.sumawi@gmail.com>
2019-05-16 16:13:38 +0700

    Initial Commit

---
+++ WatchGyroAccelerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchGyroAccelerator
+//
+//  Created by Indra Sumawi on 16/05/19.
+//  Copyright © 2019 Indra Sumawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
